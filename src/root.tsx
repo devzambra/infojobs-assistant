336542e07727684f87f0e4bd4635aea517425a0a
devzambra <dev.mzambrano@gmail.com>
2023-05-29 16:34:59 +0200

    fix: safe area

--- src/root.tsx
+++ src/root.tsx
@@ -20,11 +20,11 @@ export default component$(() => {
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
-      <body lang="en">
+      <body lang="es">
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
